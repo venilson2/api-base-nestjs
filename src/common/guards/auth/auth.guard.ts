fb22ba52091620a11bf80057f8d40ad4fff967e3
venilson2 <venilson2009@hotmail.com>
2024-10-03 11:38:35 -0300

    feat(auth-guard): description in exception of unauthorized

--- src/common/guards/auth/auth.guard.ts
+++ src/common/guards/auth/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Token not found');
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
       });
       request['user'] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid token');
     }
     return true;
   }
