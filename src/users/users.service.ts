884f8a05c3fdb49fdb8eab852cec2d0b07470e16
venilson2 <venilson2009@hotmail.com>
2024-10-02 23:45:06 -0300

    feat(users): implementend methods of services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserModel } from './models/user.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(UserModel)
+    private readonly userModel: typeof UserModel,
+  ) {}
+  
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userModel.create(createUserDto);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.findAll();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userModel.findByPk(id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.userModel.update(updateUserDto, {
+      where: { id } ,
+      returning: true
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.userModel.destroy({ where: { id } });
   }
 }
